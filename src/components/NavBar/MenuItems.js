047ff9dea0d9ead8e59813794c192dc5ecee1325
Heuller <heullergit@gmail.com>
2021-05-08 16:09:24 -0300

    first commit

---
+++ src/components/NavBar/MenuItems.js
@@ -0,0 +1,18 @@
+export const MenuItems = [
+    {
+        title: 'Calculadoras',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sobre',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'FAQ',
+        url: '#',
+        cName: 'nav-links'
+    }
+    
+];
