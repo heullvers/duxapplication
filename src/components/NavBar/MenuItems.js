c56bc0bf25aaab11431f6f81fd2cb569eaf21026
Heuller <heullergit@gmail.com>
2021-05-09 16:54:57 -0300

    footer adicionado

---
+++ src/components/NavBar/MenuItems.js
@@ -0,0 +1,26 @@
+export const MenuItems = [
+    /*
+    {
+        title: 'Calculadoras',
+        url: '#',
+        cName: 'nav-links'
+    },
+    */
+   /*
+    {
+        title: 'ENTRAR',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+    */
+    {
+        title: 'SOBRE',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'FAQ',
+        url: '#',
+        cName: 'nav-links'
+    },
+];
---
+++ src/components/pages/Footer/index.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import './index.css';
+import { Button } from '../../Button';
+import { BrowserRouter, Link } from 'react-router-dom';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin
+} from 'react-icons/fa';
+import { MdFingerprint } from 'react-icons/md';
+import Duck from './../../../images/duck.png';
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <section className='footer-subscription'>
+        <p className='footer-subscription-heading'>
+            Seja membro exclusivo da Dux para receber novidades e cupons de desconto!
+        </p>
+        <p className='footer-subscription-text'>
+          Você pode se desinscrever a qualquer momento.
+        </p>
+        <div className='input-areas'>
+          <form>
+            <input
+              className='footer-input'
+              name='email'
+              type='email'
+              placeholder='Seu email'
+            />
+            <Button buttonStyle='btn--outline'>Inscrever-se</Button>
+          </form>
+        </div>
+      </section>
+      <section className='social-media'>
+        <div className='social-media-wrap'>
+          <div className='footer-logo'>
+          <BrowserRouter><Link to='/' className='social-logo'>Dux<img alt="description" className="navbar-logo-image" src={Duck}/></Link></BrowserRouter>
+          </div>
+          <small className='website-rights'>DUX © 2021</small>
+          <div className='social-icons'>
+          <BrowserRouter><Link className='social-icon-link' to='/' target='_blank' aria-label='Facebook'><FaFacebook /></Link>
+          </BrowserRouter>
+            
+            <BrowserRouter><Link
+              className='social-icon-link'
+              to='/'
+              target='_blank'
+              aria-label='Instagram'
+            >
+              <FaInstagram />
+            </Link></BrowserRouter>
+            
+            <BrowserRouter><Link
+              className='social-icon-link'
+              to={
+                '//www.youtube.com/channel/UCsKsymTY_4BYR-wytLjex7A?view_as=subscriber'
+              }
+              target='_blank'
+              aria-label='Youtube'
+            >
+              <FaYoutube />
+            </Link></BrowserRouter>
+            
+            <BrowserRouter><Link
+              className='social-icon-link'
+              to='/'
+              target='_blank'
+              aria-label='Twitter'
+            >
+              <FaTwitter />
+            </Link></BrowserRouter>
+            
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
